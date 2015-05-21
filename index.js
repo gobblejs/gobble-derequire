2435c80e96055a18ba08d42d63bd8a3b3c11bb0b
Rich Harris <richard.a.harris@gmail.com>
2015-05-21 14:26:28 -0400

    remove inline require

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+var dereq = require( 'derequire' );
+
 module.exports = derequire;
 
 function derequire ( code, options ) {
-	var dereq = require( 'derequire' );
-
 	return options.replacements ?
 		dereq( code, options.replacements ) :
 		dereq( code, options.tokenTo || '_dereq_', options.tokenFrom || 'require' );
